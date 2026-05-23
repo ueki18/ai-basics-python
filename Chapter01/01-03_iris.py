c2ee7932a95ec95f67e2e245e3ca80dc9ff144f0
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-05-23 10:14:54 +0200

    Add files via upload

---
+++ Chapter01/01-03_iris.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+
+df = pd.DataFrame(
+    iris.data,
+    columns=iris.feature_names
+)
+
+df["species"] = [iris.target_names[i] for i in iris.target]
+
+print("データ件数:", len(df))
+print(df)
---
+++ Chapter01/01-05_iris_describe.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+
+iris = load_iris()
+
+df = pd.DataFrame(
+    iris.data,
+    columns=iris.feature_names
+)
+
+print(df.describe())
