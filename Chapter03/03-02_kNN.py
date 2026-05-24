8ba3f7e211f49a7e61b9bb5f66b6483e0c6bcc49
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-05-25 01:19:50 +0200

    Add files via upload

---
+++ Chapter03/03-02_kNN.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0
+)
+
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("予測結果:", y_pred[:10])
+print("正解ラベル:", y_test[:10])
