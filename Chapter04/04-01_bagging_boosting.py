1637ee65efe5057f518d59e048b3c76926c30b3d
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-06-05 09:34:15 +0900

    Update 04-01_bagging_boosting.py

--- Chapter04/04-01_bagging_boosting.py
+++ Chapter04/04-01_bagging_boosting.py
@@ -5,7 +5,9 @@ from sklearn.model_selection import train_test_split
 
 # データ読み込み
 X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0
+)
 
 # バギング
 bagging = BaggingClassifier(
