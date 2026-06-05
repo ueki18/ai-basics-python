b60e64d0c7ce44f49ec63062f89bee9426dcf11b
ueki18 <32954520+ueki18@users.noreply.github.com>
2026-06-05 09:36:39 +0900

    Update 04-04_XGBoost.py

--- Chapter04/04-04_XGBoost.py
+++ Chapter04/04-04_XGBoost.py
@@ -11,7 +11,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=0
 )
 
-model = XGBClassifier()
+model = XGBClassifier(random_state=0)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
